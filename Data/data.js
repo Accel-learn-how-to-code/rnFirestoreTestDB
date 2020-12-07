a9f6a4d110f17212628e125996d2864cd110e8b1
Accel <cyberpunk2076@gmail.com>
2020-12-07 16:37:45 +0700

    fixed

--- Data/data.js
+++ Data/data.js
@@ -1,15 +1,3 @@
 const animals = [];
 
-// const test = [
-//   {
-//     id: '1',
-//     name: 'long',
-//   },
-//   {
-//     id: '2',
-//     name: 'san',
-//   },
-// ];
-
-//module.exports = test;
 module.exports = animals;
