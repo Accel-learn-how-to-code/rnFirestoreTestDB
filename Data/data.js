020189953dcc98abc8a262d174edb3bda236ec16
Accel <cyberpunk2076@gmail.com>
2020-12-07 16:35:33 +0700

    first commit

---
+++ Data/data.js
@@ -0,0 +1,15 @@
+const animals = [];
+
+// const test = [
+//   {
+//     id: '1',
+//     name: 'long',
+//   },
+//   {
+//     id: '2',
+//     name: 'san',
+//   },
+// ];
+
+//module.exports = test;
+module.exports = animals;
